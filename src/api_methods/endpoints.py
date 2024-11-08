daead859c56cf6b3731adaded9bda668a8de43bd
AugustinGoubault <augustin.goubault@mines-paristech.fr>
2024-11-08 15:14:25 +0100

    reverse chnage endpoints

--- src/api_methods/endpoints.py
+++ src/api_methods/endpoints.py
@@ -1,5 +1,4 @@
 auth_endpoint = "https://www.strava.com/oauth/authorize"
 activites_endpoint = "https://www.strava.com/api/v3/athlete/activities"
 token_endpoint = "https://www.strava.com/oauth/token"
-# local_endpoint = "http://localhost:8501/"
-local_endpoint = "https://runningapp-4hywrzgxiw5gbicenbi9nh.streamlit.app/"
+local_endpoint = "http://localhost:8501/"
